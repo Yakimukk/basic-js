df134d82c1e18012d59f90841c91d238902d3220
Ekaterina Yakimuk <92802927+Yakimukk@users.noreply.github.com>
2022-05-01 23:42:29 +0300

    feat: add solution

--- src/dns-stats.js
+++ src/dns-stats.js
@@ -22,9 +22,20 @@ const { NotImplementedError } = require('../extensions/index.js');
  * }
  *
  */
-function getDNSStats(/* domains */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getDNSStats(domains) {
+  let dns = {};
+  for (let domain of domains) {
+    domain = domain.split('.').reverse();
+    let s = '';
+    for (let dom of domain) {
+        s += '.' + dom;
+        if (dns[s])
+            dns[s]++;
+        else
+            dns[s] = 1;
+    }
+  }
+  return dns;
 }
 
 module.exports = {
